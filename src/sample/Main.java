4e1cb3557e9448dbd4961a836b54867f63e35f61
weoz <weoz@ukr.net>
2016-11-09 11:34:03 +0200

    created main window(main.fxml)

---
+++ src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setMinHeight(550);
+        primaryStage.setMinWidth(400);
+        primaryStage.setScene(new Scene(root, 350, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
