015273537954bdc3edc0851483c5352dc08ef561
weoz <weoz@ukr.net>
2016-11-09 15:19:31 +0200

    fx:id binding to the fields in the controller via @FXML annotation

--- src/ua/kruart/address_book/controller/EditDialogController.java
+++ src/ua/kruart/address_book/controller/EditDialogController.java
@@ -1,7 +1,19 @@
 package ua.kruart.address_book.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 /**
  * Created by Arthur on 09.11.2016.
  */
 public class EditDialogController {
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtPhone;
+    @FXML
+    private Button btnOk;
+    @FXML
+    private Button btnCancel;
 }
--- src/ua/kruart/address_book/controller/MainController.java
+++ src/ua/kruart/address_book/controller/MainController.java
@@ -1,17 +1,35 @@
 package ua.kruart.address_book.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainController {
-
+    @FXML
+    private Button btnAdd;
+    @FXML
+    private Button btnEdit;
+    @FXML
+    private Button btnDelete;
+    @FXML
+    private Button btnSearch;
+    @FXML
+    private TextField txtSearch;
+    @FXML
+    private TableView tableAddressBook;
+    @FXML
+    private Label labelCount;
 
     public void showDialog(ActionEvent actionEvent) {
 
